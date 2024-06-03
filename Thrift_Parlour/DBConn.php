06058ea5b8f0b1f17ca4a34122d0b424140cb830
LouisAmphibian <ayandamantshinga30@gmail.com>
2024-06-03 20:43:19 +0200

    better connection

--- Thrift_Parlour/DBConn.php
+++ Thrift_Parlour/DBConn.php
@@ -8,6 +8,6 @@ $database = (string) "thriftparlour";
 $port_number = (int) 3306;
 
 // Establish connection to MySQL database
-$connection = mysqli_connect( $server_name , $username, $password, $database) or die("Couldn't connect ");
+$connection = mysqli_connect( $server_name , $username, $password, $database, $port_number) or die("Couldn't connect ");
 
 ?>
